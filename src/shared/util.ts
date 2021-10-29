9c082e352addfa86b96d9f9517c4fa3f685550f2
Brendan Whiting <brendanwhiting@Brendans-MacBook-Pro.local>
2021-10-28 18:16:15 -0700

    convert 1/2 to 0.5 for sorting

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -1,18 +1,23 @@
 import { Todo } from "./todo.interface";
 
 export const padUrlWithHttp = (url: string) => {
-  if (url.startsWith("http")) return url;
-  return `http://${url}`;
+    if (url.startsWith("http")) return url;
+    return `http://${url}`;
 };
 
 export const prettifyPoms = (poms: string) => {
-  return poms === "0.5" || poms === "1/2" ? "½" : poms;
+    return poms === "0.5" || poms === "1/2" ? "½" : poms;
 };
 
+const convertStringPoms = (poms: string) => {
+    if (poms === '1/2') return 0.5;
+    return parseFloat(poms);
+}
+
 export const sortTodos = (a: Todo, b: Todo) => {
-  if (a.done === b.done) {
-    return parseFloat(a.poms) - parseFloat(b.poms);
-  } else {
-    return Number(a.done) - Number(b.done);
-  }
+    if (a.done === b.done) {
+        return convertStringPoms(a.poms) - convertStringPoms(b.poms);
+    } else {
+        return Number(a.done) - Number(b.done);
+    }
 };
