a1820f7610878a6d83c7dd2a9904bb2e0046c1a5
Brendan Whiting <brendanwhiting@Brendans-MacBook-Pro.local>
2021-10-29 11:17:04 -0700

    format

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -3,50 +3,50 @@ import { generateNewTodo } from "../shared/util";
 import { RootState } from "./store";
 
 const handleMigration = (state: any) => {
-    // console.log('state.todos.pastTodos', state.todos.pastTodos);
-    // if (state.todos.pastTodos) {
-    const newShape = {
-        todos: {
-            pastState: state.todos.pastTodos.map((todoList: Todo[]) => {
-                return {
-                    todos: todoList,
-                    newTodo: generateNewTodo(),
-                };
-            }),
-            currentState: {
-                todos: state.todos,
-                newTodo: generateNewTodo(),
-            },
-            futureState: state.todos.futureTodos.map((todoList: Todo[]) => {
-                return {
-                    todos: todoList,
-                    newTodo: generateNewTodo(),
-                };
-            }),
-        }
-    }
-    console.log('newShape', newShape);
-    return newShape;
+  // console.log('state.todos.pastTodos', state.todos.pastTodos);
+  // if (state.todos.pastTodos) {
+  const newShape = {
+    todos: {
+      pastState: state.todos.pastTodos.map((todoList: Todo[]) => {
+        return {
+          todos: todoList,
+          newTodo: generateNewTodo(),
+        };
+      }),
+      currentState: {
+        todos: state.todos,
+        newTodo: generateNewTodo(),
+      },
+      futureState: state.todos.futureTodos.map((todoList: Todo[]) => {
+        return {
+          todos: todoList,
+          newTodo: generateNewTodo(),
+        };
+      }),
+    },
+  };
+  console.log("newShape", newShape);
+  return newShape;
 };
 
 export const loadState = () => {
-    try {
-        const serializedState = localStorage.getItem("state");
-        if (serializedState === null) {
-            return undefined;
-        }
-        return handleMigration(JSON.parse(serializedState));
-    } catch (err) {
-        console.log('Im here')
-        return undefined;
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
     }
+    return handleMigration(JSON.parse(serializedState));
+  } catch (err) {
+    console.log("Im here");
+    return undefined;
+  }
 };
 
 export const saveState = (state: RootState) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem("state", serializedState);
-    } catch (err) {
-        // ignore write errors
-    }
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    // ignore write errors
+  }
 };
