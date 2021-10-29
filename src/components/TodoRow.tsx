7c073fcd190606572429aa0373a3c840558e5501
Brendan Whiting <brendanwhiting@Brendans-MacBook-Pro.local>
2021-10-29 13:57:17 -0700

    make text strikethrough for done

--- src/components/TodoRow.tsx
+++ src/components/TodoRow.tsx
@@ -10,77 +10,77 @@ import { MoveUpIconButton } from "./icon-buttons/MoveUpIconButton";
 import { Due } from "../shared/due.type";
 
 interface Props {
-  todo: Todo;
-  onSetTodoDone: (todo: Todo, done: boolean) => void;
-  onEditName: (todo: Todo, newName: string) => void;
-  onEditPoms: (todo: Todo, newPoms: string) => void;
-  onMoveTodoDue: (todo: Todo) => void;
-  onDeleteTodo: (id: ID) => void;
-  onEditLink: (todo: Todo, linkId: ID, newUrl: string) => void;
-  onDeleteLink: (todo: Todo, linkId: ID) => void;
-  due: Due;
+    todo: Todo;
+    onSetTodoDone: (todo: Todo, done: boolean) => void;
+    onEditName: (todo: Todo, newName: string) => void;
+    onEditPoms: (todo: Todo, newPoms: string) => void;
+    onMoveTodoDue: (todo: Todo) => void;
+    onDeleteTodo: (id: ID) => void;
+    onEditLink: (todo: Todo, linkId: ID, newUrl: string) => void;
+    onDeleteLink: (todo: Todo, linkId: ID) => void;
+    due: Due;
 }
 
 export function TodoRow({
-  todo,
-  onSetTodoDone,
-  onEditName,
-  onEditPoms,
-  onMoveTodoDue,
-  onDeleteTodo,
-  onEditLink,
-  onDeleteLink,
-  due,
+    todo,
+    onSetTodoDone,
+    onEditName,
+    onEditPoms,
+    onMoveTodoDue,
+    onDeleteTodo,
+    onEditLink,
+    onDeleteLink,
+    due,
 }: Props) {
-  const { id, done, name, poms, links, archivedDate } = todo;
-  return (
-    <tr key={id}>
-      <td className="done">
-        {due === Due.Archived ? (
-          <div>{new Date(archivedDate || "")?.toLocaleDateString("en-US")}</div>
-        ) : (
-          <Form.Check
-            type="checkbox"
-            checked={done}
-            onChange={(e: any) => onSetTodoDone(todo, e.target.checked)}
-          />
-        )}
-      </td>
-      <td className="name">
-        <TextField
-          text={name}
-          onEditText={(newName) => onEditName(todo, newName)}
-        />
-      </td>
-      <td className="poms">
-        <TextField
-          text={poms}
-          onEditText={(newPoms) => onEditPoms(todo, newPoms)}
-        />
-      </td>
-      <td className="links">
-        <div>
-          {links.map(({ id, url }, i) => (
-            <Link
-              key={id}
-              url={url}
-              onEditLink={(newUrl) => onEditLink(todo, id, newUrl)}
-              onDeleteLink={() => onDeleteLink(todo, id)}
-            />
-          ))}
-        </div>
-      </td>
-      <td className="actions">
-        <ButtonGroup>
-          {due === Due.Today ? (
-            <MoveDownIconButton onClick={() => onMoveTodoDue(todo)} />
-          ) : (
-            <MoveUpIconButton onClick={() => onMoveTodoDue(todo)} />
-          )}
+    const { id, done, name, poms, links, archivedDate } = todo;
+    return (
+        <tr key={id} className={done ? 'complete' : ''}>
+            <td className="done">
+                {due === Due.Archived ? (
+                    <div>{new Date(archivedDate || "")?.toLocaleDateString("en-US")}</div>
+                ) : (
+                    <Form.Check
+                        type="checkbox"
+                        checked={done}
+                        onChange={(e: any) => onSetTodoDone(todo, e.target.checked)}
+                    />
+                )}
+            </td>
+            <td className="name">
+                <TextField
+                    text={name}
+                    onEditText={(newName) => onEditName(todo, newName)}
+                />
+            </td>
+            <td className="poms">
+                <TextField
+                    text={poms}
+                    onEditText={(newPoms) => onEditPoms(todo, newPoms)}
+                />
+            </td>
+            <td className="links">
+                <div>
+                    {links.map(({ id, url }, i) => (
+                        <Link
+                            key={id}
+                            url={url}
+                            onEditLink={(newUrl) => onEditLink(todo, id, newUrl)}
+                            onDeleteLink={() => onDeleteLink(todo, id)}
+                        />
+                    ))}
+                </div>
+            </td>
+            <td className="actions">
+                <ButtonGroup>
+                    {due === Due.Today ? (
+                        <MoveDownIconButton onClick={() => onMoveTodoDue(todo)} />
+                    ) : (
+                        <MoveUpIconButton onClick={() => onMoveTodoDue(todo)} />
+                    )}
 
-          <DeleteIconButton onClick={() => onDeleteTodo(todo.id)} />
-        </ButtonGroup>
-      </td>
-    </tr>
-  );
+                    <DeleteIconButton onClick={() => onDeleteTodo(todo.id)} />
+                </ButtonGroup>
+            </td>
+        </tr>
+    );
 }
