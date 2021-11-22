b0e6430ffca8a86a97affb0ee12314509e0127d9
Brendan Whiting <brendanwhiting@Brendans-MacBook-Pro.local>
2021-11-22 13:13:07 -0800

    fix lint

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -28,7 +28,6 @@ function Todos() {
   const {
     showArchive,
     setShowArchive,
-    showAnimation,
     setShowAnimation
   } = useAppContext();
   const { sortTodosWithToast, onArchiveAllCompletedTodosWithToast, addNewTodoAndStartEditing, addTodoFromTemplateWithToast } = useReduxActionsWithContext();
