a26b915ea22a3fea32930c2667d7fc93a5cbf960
Brendan Whiting <brendanwhiting@Brendans-MacBook-Pro.local>
2021-11-29 13:27:18 -0800

    revert button group

--- src/components/HoverDropdown.tsx
+++ src/components/HoverDropdown.tsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, Dropdown } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
 
 interface Props {
   toggleText: string;
@@ -19,7 +19,6 @@ export const HoverDropdown: React.FC<Props> = ({
       show={show}
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
-      as={ButtonGroup}
     >
       <Dropdown.Toggle
         variant="outline-primary"
